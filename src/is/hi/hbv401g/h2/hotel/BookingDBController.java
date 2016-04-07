e9de5f3bf93f91848cb5fef45c6ed0f207760f73
monsiman <simon-rafn@hotmail.com>
2016-04-07 18:29:53 +0000

    small changes created skeleton for BookingDBController class

--- src/is/hi/hbv401g/h2/hotel/BookingDBController.java
+++ src/is/hi/hbv401g/h2/hotel/BookingDBController.java
@@ -1,5 +1,37 @@
 package is.hi.hbv401g.h2.hotel;
 
+import java.util.Date;
+
 public class BookingDBController {
 
+	// NOT FINISHED
+	Booking[] getBookings() {
+		/* NEEDS LOGIC */
+		return null;
+	}
+	
+	// NOT FINISHED
+	Booking[] getBookingsByTraveler(Traveler traveler) {
+		/* NEEDS LOGIC */
+		return null;
+	}
+	
+	// NOT FINISHED
+	void cancelBooking(Booking booking) {
+		/* NEEDS LOGIC */
+	}
+	
+	// NOT FINISHED
+	Booking editBooking(Booking booking, Date toDate, Date fromDate) {
+		/* NEEDS LOGIC */
+		return null;
+	}
+	
+	// NOT FINISHED
+	void book(Booking booking) {
+		/* NEEDS LOGIC */
+	}
+	
+	// NOT FINISHED
+	
 }
