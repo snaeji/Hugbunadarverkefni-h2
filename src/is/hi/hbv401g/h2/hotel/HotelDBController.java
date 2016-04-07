aeede5df6805221ad3a312deca4b002bc9838197
snaebjorn.valur <snaebjorn.valur@gmail.com>
2016-04-07 03:10:37 +0000

    db komid adeins afram

--- src/is/hi/hbv401g/h2/hotel/HotelDBController.java
+++ src/is/hi/hbv401g/h2/hotel/HotelDBController.java
@@ -1,18 +1,29 @@
 package is.hi.hbv401g.h2.hotel;
+import java.sql.*;
 
 public class HotelDBController {
+
 
 	
 	// NOT FINISHED
 	public Room[] searchWithAddress(int minPrice,int maxPrice,int minStars,int maxStars,String[] amenities, String street, String city, String zipCode){
-		
 		Room[] room = new Room[0];
+		ResultSet rs = Database.executeQuery(1,"a");
+//		while ( rs.next() ) {
+		// Here is a loop where we want to add to the room object
+//			minPrice = rs.getInt("minprice");
+//			city = rs.getString("city");
+//		}
+			
+		
 		return room;
 		
 	};
 
 	// NOT FINISHED
 	public Room[] searchWithCoords(int minPrice,int maxPrice,int minStars,int maxStars,String[] amenities, float radius, Coordinates coords){
-		return hotelDBController.searchWithCoords(minPrice,maxPrice,minStars,maxStars,amenities,radius,coords);
-	};
+		ResultSet rs = Database.executeQuery(1,"a");
+		Room[] room = new Room[0];
+		return room;
+		};
 }
